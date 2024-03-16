24c3ba10f8a6bd4066801c2b7e4b0fadbee1f564
vanntoengcaptaincambodia <toeng.vann@institute.pse.ngo>
2024-03-16 09:05:50 +0700

    articles card

---
+++ exercises/Exercise-1/ex1.js
@@ -0,0 +1,10 @@
+// Creating a new paragraph element
+const newParagraph = document.createElement('p');
+
+// Setting the text content of the new paragraph
+newParagraph.textContent = 'This is a new paragraph.';
+
+// Appending the new paragraph to the body of the document
+document.body.appendChild(newParagraph);
+
+newParagraph.innerText = "Hello Kon papa"
---
+++ exercises/Exercise-1/javacripts/script.js
@@ -0,0 +1,22 @@
+// Define variables to store information
+var img1 = "images/profile.jpeg";
+var img2 = "images/profile1.jpeg";
+
+// Please replace your name
+var name1 = "Trithay";
+var name2 = "Vann Cap";
+
+var position1 = "Front-end developer";
+var position2 = "Back-end developer";
+
+var desc1 = "He's a software engineer working for Microsoft company";
+var desc2 = "He's a software engineer working for Oracle company";
+
+document.getElementById("img1").src= img1
+document.getElementById("profile_name1").innerHTML=name1;
+document.getElementById("position1").innerHTML=position1;
+
+document.getElementById("img2").src= img1
+document.getElementById("profile_name2").innerHTML=name2;
+document.getElementById("position2").innerHTML=position2;
+
